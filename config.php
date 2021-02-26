fe7d1776bd53f55f60fb90829139748a3e44d0c3
Niko-star <52108519+Niko-star@users.noreply.github.com>
2021-02-26 19:44:10 +0300

    Add files via upload
    forntana

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+
+//General
+$APIKey = "7B8A5D69856F7E3D77362088BFD2006A"; // Go get your API here: http://steamcommunity.com/dev/apikey
+
+$load_name ="Star Wars"; // For your Browser Name
+$community_name ="Star Wars"; // Your community/server name. The one in the middle
+$community_url = "https://vk.com/public202558481"; // Url for your users to know where to go
+$intro = TRUE; // If you want to turn this off, type FALSE instead
+// Introduction
+$tagline = " Давным-давно, на сервере далеко-далеко...."; // The intro text in the beginning of the animation
+// Above Server Name
+$sub_title_1 = "Добро пожаловать"; // Above your server name
+// Under Server Name
+$sub_title_2 = "читайте правила, чтобы узнать пути силы"; // Below your server name
+
+//Bottom Part
+$left_bottom_title = "о нас";
+$left_bottom_sub_title = "$community_url discord";
+$right_bottom_title = "Наш Сервер";
+$right_bottom_sub_title = "Общая информация о сервере";
+
+$left_bottom_text = "Мы стараемся для.Развитием проекта";
+
+?>
